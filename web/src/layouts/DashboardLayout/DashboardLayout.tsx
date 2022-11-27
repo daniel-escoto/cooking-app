58271aa56a9c13d8e9194124e78806d1957f49fc
Daniel Escoto <escotodanny62@gmail.com>
2022-11-27 11:26:35 -0800

    fix layout

--- web/src/layouts/DashboardLayout/DashboardLayout.tsx
+++ web/src/layouts/DashboardLayout/DashboardLayout.tsx
@@ -10,7 +10,7 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
       <NavBar />
       <main className="flex h-screen w-full flex-col items-center justify-center bg-white dark:bg-gray-900 dark:text-white">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex min-h-screen max-w-7xl flex-col items-center px-4 py-8 sm:px-6 lg:px-8">
+          <div className="mx-auto flex min-h-screen max-w-7xl flex-col items-center px-4 py-8 sm:px-6 lg:px-8">
             {children}
           </div>
         </div>
